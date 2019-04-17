692fa6521fe13f421165c3d72c50c514796fed4e
Emanuel4168 <emanuel.l4168@gmail.com>
2019-04-17 12:29:06 -0600

    Creacion de la clase Estacion

---
+++ Planta-Ensambladora/src/plantaEnsambladora/Estacion.java
@@ -0,0 +1,46 @@
+package plantaEnsambladora;
+
+public class Estacion extends Thread{
+
+	private String stationName;
+	private int operationTime;
+	private boolean produce;
+	
+	public Estacion(String name, int operationTime, boolean produce) {
+		super();
+		this.stationName = name;
+		this.operationTime = operationTime;
+		this.produce = produce;
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+
+	public String getStationName() {
+		return stationName;
+	}
+
+	public void setStationName(String name) {
+		this.stationName = name;
+	}
+
+	public int getOperationTime() {
+		return operationTime;
+	}
+
+	public void setOperationTime(int operationTime) {
+		this.operationTime = operationTime;
+	}
+
+	public boolean isProduce() {
+		return produce;
+	}
+
+	public void setProduce(boolean produce) {
+		this.produce = produce;
+	}
+	
+	
+}
